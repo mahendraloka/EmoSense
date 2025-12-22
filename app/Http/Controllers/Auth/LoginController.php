f89c04f904ce19f581ef279228892124e84d3b4e
iniduta <inidutabukandia@gmail.com>
2025-12-22 16:39:02 +0700

    login auth

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -10,7 +10,7 @@ class LoginController extends Controller
 {
     public function showLogin()
     {
-        return view('auth.login');
+        return view('/login');
     }
 
     // public function login(Request $request)
